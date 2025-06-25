50ea8133f37a7a06ddf60569a55f32dc7fee241b
hxshen <hxshen@siena.edu>
2025-06-25 16:19:33 -0400

    HELPHELP idk how to do the drlbf testing :(

--- BibleTestingSuite.py
+++ BibleTestingSuite.py
@@ -1,41 +1,55 @@
-import csv
-from nltk.tag import pos_tag
-from nltk.tokenize import word_tokenize
+import numpy as np
 import pickle
 
-def alphaify(string):
-    return "".join([c for c in string if c.isalpha()]).lower()
-#
-# path = "archive/t_kjv.csv"
-#
-# bible_corpus = []
-# bible_corpus_nouns = []
-# bible_corpus_notnouns = []
-#
-# with open(path, 'r', newline='') as csvfile:
-#     csv_reader = csv.reader(csvfile)
-#
-#     header = next(csv_reader)
-#
-#     for row in csv_reader:
-#         for i in row[4].split(" "):
-#             entry = pos_tag(word_tokenize(alphaify(i)))
-#             if entry != []:
-#                 bible_corpus.append(entry[0][0])
-#                 if entry[0][1] == "NN":
-#                     bible_corpus_nouns.append(entry[0][0])
-#                 else:
-#                     bible_corpus_notnouns.append(entry[0][0])
-#
-# with open('bible_corpus.pkl', 'wb') as f:
-#     pickle.dump(bible_corpus, f)
-#
-# with open('bible_corpus_nouns.pkl', 'wb') as f:
-#     pickle.dump(bible_corpus_nouns, f)
-#
-# with open('bible_corpus_notnouns.pkl', 'wb') as f:
-#     pickle.dump(bible_corpus_notnouns, f)
-with open('bible_corpus.pkl', 'rb') as f:
-    loaded_obj = pickle.load(f)
+from LearnedBloomFilter import BloomFilter, CountingBloomFilter, LearnedBloomFilter
+from DRLearnedBloomFilter import DRLearnedBloomFilter
 
-print(loaded_obj)
+# fixing memory !!!
+memory = 10000
+n_hash = 5
+
+scale = 2000
+
+bf_params = (n_hash, memory // n_hash) # (k, m)
+
+# Memory usage = k * m
+
+lbf_params = (n_hash, memory // n_hash, lambda : GradientBoostingClassifier(n_estimators = 10), lambda x : [x/scale]) # (k, m, confidence_threshold)
+
+# Memory usage = k * m + 1 ~~ km
+
+drlbf_params = (2, 1000, n_hash, memory // (3 * n_hash), lambda : GradientBoostingClassifier(n_estimators = 10), lambda x : [x/scale]) # (C, N, k, m, model)
+
+# Memory usage = (k * m) * (C + 1) + C * 1 ~~ (C + 1)(km + 1) ~~ (C + 1)km
+# 3km in this case
+
+def get_arg_q_thingy(dbf):
+	return dbf.B[np.argmax(dbf.W)].n if len(dbf.W) != 0 else -1
+
+with open('bible_corpus.pkl', 'rb') as f:
+    bible_corpus = pickle.load(f)
+with open('bible_corpus_nouns.pkl', 'rb') as f:
+    bible_corpus_nouns = pickle.load(f)
+with open('bible_corpus_notnouns.pkl', 'rb') as f:
+    bible_corpus_notnouns = pickle.load(f)
+
+
+def test(lbf_params, bf_params, drlbf_params, FPR_batch_size=1000, num_batch=500, local_scale=1000, scale=2000, dshift_rate=100, n_intervals = 5):
+    bf = BloomFilter(*bf_params)
+    lbf = LearnedBloomFilter(*lbf_params)
+    drlbf = DRLearnedBloomFilter(*drlbf_params)
+
+    bf_fpr = []
+    lbf_fpr = []
+    drlbf_fpr = []
+
+    def query_true(word):
+        if word in bible_corpus_nouns:
+            return True
+    bf.insert(word)
+    drlbf.insert(word)
+    lbf.train(word)#help idk wat goes in here
+    #dr lbf stuff here
+
+for word in bible_corpus:
+    test(word) #idk what params we gonna put here
